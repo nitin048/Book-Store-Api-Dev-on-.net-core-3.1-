81480ecfd747741f2a3b5cfd472baabbdf1c40d3
nitin pradhan <nitinpradhan48@gmail.com>
2020-11-16 14:52:37 +0530

    day 1 , commit 2

---
+++ books-api/Contracts/ILoggerService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace books_api.Contracts
+{
+    public interface ILoggerService
+    {
+        void logInfo(string message);
+        void logWarn(string message);
+        void logError(string message);
+        void logDebug(string message);
+        
+    }
+}
