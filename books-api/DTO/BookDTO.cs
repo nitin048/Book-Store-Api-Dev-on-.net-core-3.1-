40f468c18c7d646c0cd22a845bc2738772ed9816
nitin pradhan <nitinpradhan48@gmail.com>
2020-11-26 13:12:45 +0530

    day 6 , commit 1

--- books-api/DTO/BookDTO.cs
+++ books-api/DTO/BookDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,5 +19,38 @@ namespace books_api.DTO
 
         public virtual AuthorDTO Author { get; set; }
 
+    }
+
+    public class BookCreateDTO
+    {
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+        [Required]
+        public string Summary { get; set; }
+        public string Image { get; set; }
+        public double? Price { get; set; }
+        [StringLength(500)]
+        public string Isbn { get; set; }
+
+        [Required]
+        public int? AuthorId { get; set; }
+    }
+    public class BookUpdateDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+        [Required]
+        public string Summary { get; set; }
+        public string Image { get; set; }
+        public double? Price { get; set; }
+        [StringLength(500)]
+        public string Isbn { get; set; }
+
+        [Required]
+        public int? AuthorId { get; set; }
     }
 }
--- books-api/Mapping/Maps.cs
+++ books-api/Mapping/Maps.cs
@@ -17,6 +17,8 @@ namespace books_api.Mapping
             CreateMap<Author, AuthorCreateDTO>().ReverseMap();
            CreateMap<Author, AuthorUpdateDTO>().ReverseMap();
             CreateMap<Book, BookDTO>().ReverseMap();
+            CreateMap<Book, BookCreateDTO>().ReverseMap();
+            CreateMap<Book, BookUpdateDTO>().ReverseMap();
         }
      
     }
