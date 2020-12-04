e4a6bfdae328049b84d9110437c37d0d43af544e
nitin pradhan <nitinpradhan48@gmail.com>
2020-12-04 12:40:09 +0530

    Day 9 ,commit 3

---
+++ BookStore-UI/Static/EndPoints.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_UI.Static
+{
+    public static class EndPoints
+    {
+        public static string BaseUrl = "https://localhost:64250/";
+
+        public static string AuthorsEndpoints = $"{BaseUrl}api/authors";
+        
+        public static string BooksEndpoints = $"{BaseUrl}api/books";
+    }
+}
